c6db12e026ded99d8d17c8792cc056a758110e94
KristaViander <48218@edu.raseko.fi>
2023-03-10 15:09:07 +0200

    lisää

--- test_questions.py
+++ test_questions.py
@@ -1,53 +1,99 @@
+# questions.py tests can be found here
+# ------------------------------------
+
 import questions
 
 # Test if conversion to integer works as expected
- 
+# def test_ask_user_integer(monkeypatch): # Simulate user input using Monkeypathc library
+#     user_input = '100'
+#     monkeypatch.setattr('builtins.input', lambda _: user_input) # Use anonymous function to create input from variable
+#     question = questions.Question('Anna kokonaisluku')
+#     assert question.ask_user_integer(False) == (100, 'OK', 0, 'Conversion successful')
 
-def test_ask_user_integer(monkeypatch): # argumenttina monkeypatch-moduli (venv - pytest)
+# # Test an error condition when user adds a unit to a number
+# def test_ask_user_integer2(monkeypatch):
+#     user_input = '100 v'
+#     monkeypatch.setattr('builtins.input', lambda _: user_input) # korvataan järjestelmän input() muuttujalla syote
+#     question = questions.Question('Anna kokonaisluku')
+#     assert question.ask_user_integer(False) == (0, 'Error', 1, "invalid literal for int() with base 10: '100 v'")
+
+# Test static conversion method to integer
+
+
+def test_static_ask_user_integer(monkeypatch):
     user_input = '100'
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_integer(False) == (100, 'OK', 0, 'Conversion successful')
+    assert questions.Question.ask_user_integer(
+        'Anna kokonaisluku', False) == (100, 'OK', 0, 'Conversion successful')
 
-# Test an error confition when user adds a unit to a number
-def test_askuser_integer2(monkeypatch):
-    user_input = '100 v'
-    monkeypatch.setattr('builtins.input', lambda _: user_input) # Korvataan järjestelmän input() muuttujalla
-    question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_integer(False) == (0, 'Error', 1, "invalid literal for int() with base 10: '100 v'")
+# Test if conversion to float works as expected
 
-def test_askuser_float(monkeypatch):
+
+# Simulate user input using Monkeypathc library
+def test_ask_user_float(monkeypatch):
     user_input = '1.5'
-    monkeypatch.setattr('builtins.input', lambda _: user_input) # Korvataan järjestelmän input() muuttujalla
+    # Use anonymous function to create input from variable
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
     question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_float(False) == (1.5, 'OK', 0, 'Conversion successful')
+    assert question.ask_user_float(False) == (
+        1.5, 'OK', 0, 'Conversion successful')
 
-def test_askuser_float2(monkeypatch):
+# Test an error condition when user adds a unit to a floating point number
+
+
+# Simulate user input using Monkeypathc library
+def test_ask_user_float2(monkeypatch):
     user_input = '1.5v'
-    monkeypatch.setattr('builtins.input', lambda _: user_input) # Korvataan järjestelmän input() muuttujalla
+    # Use anonymous function to create input from variable
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
     question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_float(False) == (0, 'Error', 1, "could noot convert string to float '1.5v'")
+    assert question.ask_user_float(False) == (
+        0, 'Error', 1, "could not convert string to float: '1.5v'")
 
-def test_askuser_float3(monkeypatch):
+# Test an error condition when user uses comma instead of dot as decimal separator
+
+
+# Simulate user input using Monkeypathc library
+def test_ask_user_float3(monkeypatch):
     user_input = '74,6'
-    monkeypatch.setattr('builtins.input', lambda _: user_input) # Korvataan järjestelmän input() muuttujalla
+    # Use anonymous function to create input from variable
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
     question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_float(False) == (0, 'Error', 1, "could not convert string to float: '74,6'")
+    assert question.ask_user_float(False) == (
+        0, 'Error', 1, "could not convert string to float: '74,6'")
 
-def test_askuser_boolean(monkeypatch):
+# Test conversion to boolean: case Y
+
+
+# Simulate user input using Monkeypathc library
+def test_ask_user_boolean(monkeypatch):
     user_input = 'y'
-    monkeypatch.setattr('builtins.input', lambda _: user_input) # Korvataan järjestelmän input() muuttujalla
+    # Use anonymous function to create input from variable
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
     question = questions.Question('Haluatko jatkaa')
-    assert question.ask_user_boolean('Y', 'N', False) == (True, 'OK', 0, 'Conversion successful')
+    assert question.ask_user_boolean('Y', 'N', False) == (
+        True, 'OK', 0, 'Conversion successful')
 
-def test_askuser_boolean2(monkeypatch):
+# Test conversion to boolean: case N
+
+
+# Simulate user input using Monkeypathc library
+def test_ask_user_boolean2(monkeypatch):
     user_input = 'n'
-    monkeypatch.setattr('builtins.input', lambda _: user_input) # Korvataan järjestelmän input() muuttujalla
+    # Use anonymous function to create input from variable
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
     question = questions.Question('Haluatko jatkaa')
-    assert question.ask_user_boolean('Y', 'N', False) == (False, 'OK', 0, 'Conversion successful')
+    assert question.ask_user_boolean('Y', 'N', False) == (
+        False, 'OK', 0, 'Conversion successful')
 
-def test_askuser_boolean3(monkeypatch):
+# Test an error condition
+
+
+# Simulate user input using Monkeypathc library
+def test_ask_user_boolean3(monkeypatch):
     user_input = 'v'
-    monkeypatch.setattr('builtins.input', lambda _: user_input) # Korvataan järjestelmän input() muuttujalla
+    # Use anonymous function to create input from variable
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
     question = questions.Question('Haluatko jatkaa')
-    assert question.ask_user_boolean('Y', 'N', False) == ('N/A', 'Error', 1, 'unable to convert to boolean')
+    assert question.ask_user_boolean('Y', 'N', False) == (
+        'N/A', 'Error', 1, 'unable to convert to boolean')
